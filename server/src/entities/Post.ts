cdf3b1ce093046be0d150b627f169ef2eefd6d57
Tom <tom@MacBook-Pro-de-Tom-2.local>
2020-09-24 22:44:05 -0500

    :sparkles: Adding express and graphql

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,18 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
-
+  @Field(() => Int)
   @PrimaryKey()
   id!: number;
 
-  @Property()
+  @Field(() => String)
+  @Property({ type: "date" })
   createdAt = new Date();
 
-  @Property({ onUpdate: () => new Date() })
+  @Field(() => String)
+  @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
-  @Property()
+  @Field()
+  @Property({ type: "text" })
   title!: string;
-
 }
